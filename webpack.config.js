1889416a2f819d991586e1e987fe9b1590044265
Lionel T <elrumordelaluz@hotmail.com>
2017-12-28 13:13:32 +0100

    Initial with all necessary files

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'postcss-loader'],
+        }),
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        loader: 'file-loader?name=public/fonts/[name].[ext]',
+      },
+      {
+        test: /\.(jpg|png|svg)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 25000,
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new ExtractTextPlugin('styles.css'),
+    new CopyWebpackPlugin([{ from: './src/images', to: 'images' }]),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+  ],
+}
